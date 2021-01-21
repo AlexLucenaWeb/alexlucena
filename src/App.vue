0a3dfd0fa08512ebbe8b678115c674d0484d85c7
janoweb <janoweb@users.noreply.github.com>
2021-01-21 23:47:22 +0000

    static backgoround section

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
-  <section></section>
+  <h1>HELLO IM ALEX</h1>
+  <static-background></static-background>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum malesuada velit, in rhoncus
+    lorem faucibus quis. Maecenas ullamcorper porttitor diam. Class aptent taciti sociosqu ad litora
+    torquent per conubia nostra, per inceptos himenaeos. Donec id luctus lectus. Fusce laoreet
+    libero diam, sed malesuada purus dignissim ac. Pellentesque dapibus volutpat luctus. Fusce
+    finibus faucibus est, vel posuere diam aliquet quis. Aliquam erat volutpat. Quisque euismod
+    pretium orci sed tincidunt. Aliquam erat volutpat. Nunc mattis sapien arcu, eget sodales neque
+    ornare sed. Praesent bibendum porttitor eros eget iaculis.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum malesuada velit, in rhoncus
+    lorem faucibus quis. Maecenas ullamcorper porttitor diam. Class aptent taciti sociosqu ad litora
+    torquent per conubia nostra, per inceptos himenaeos. Donec id luctus lectus. Fusce laoreet
+    libero diam, sed malesuada purus dignissim ac. Pellentesque dapibus volutpat luctus. Fusce
+    finibus faucibus est, vel posuere diam aliquet quis. Aliquam erat volutpat. Quisque euismod
+    pretium orci sed tincidunt. Aliquam erat volutpat. Nunc mattis sapien arcu, eget sodales neque
+    ornare sed. Praesent bibendum porttitor eros eget iaculis.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum malesuada velit, in rhoncus
+    lorem faucibus quis. Maecenas ullamcorper porttitor diam. Class aptent taciti sociosqu ad litora
+    torquent per conubia nostra, per inceptos himenaeos. Donec id luctus lectus. Fusce laoreet
+    libero diam, sed malesuada purus dignissim ac. Pellentesque dapibus volutpat luctus. Fusce
+    finibus faucibus est, vel posuere diam aliquet quis. Aliquam erat volutpat. Quisque euismod
+    pretium orci sed tincidunt. Aliquam erat volutpat. Nunc mattis sapien arcu, eget sodales neque
+    ornare sed. Praesent bibendum porttitor eros eget iaculis.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum malesuada velit, in rhoncus
+    lorem faucibus quis. Maecenas ullamcorper porttitor diam. Class aptent taciti sociosqu ad litora
+    torquent per conubia nostra, per inceptos himenaeos. Donec id luctus lectus. Fusce laoreet
+    libero diam, sed malesuada purus dignissim ac. Pellentesque dapibus volutpat luctus. Fusce
+    finibus faucibus est, vel posuere diam aliquet quis. Aliquam erat volutpat. Quisque euismod
+    pretium orci sed tincidunt. Aliquam erat volutpat. Nunc mattis sapien arcu, eget sodales neque
+    ornare sed. Praesent bibendum porttitor eros eget iaculis.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum malesuada velit, in rhoncus
+    lorem faucibus quis. Maecenas ullamcorper porttitor diam. Class aptent taciti sociosqu ad litora
+    torquent per conubia nostra, per inceptos himenaeos. Donec id luctus lectus. Fusce laoreet
+    libero diam, sed malesuada purus dignissim ac. Pellentesque dapibus volutpat luctus. Fusce
+    finibus faucibus est, vel posuere diam aliquet quis. Aliquam erat volutpat. Quisque euismod
+    pretium orci sed tincidunt. Aliquam erat volutpat. Nunc mattis sapien arcu, eget sodales neque
+    ornare sed. Praesent bibendum porttitor eros eget iaculis.
+  </p>
 </template>
 
 <script>
+import StaticBackground from './components/StaticBackground.vue';
 // import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
+    StaticBackground,
     // HelloWorld,
   },
 };
 </script>
 
 <style>
+@font-face {
+  font-family: jeko;
+  src: url("../public/fonts/Jeko-Extra-Bold.ttf"), url("../public/fonts/Jeko-Regular.ttf");
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: jeko, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/StaticBackground.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="bg__back">
+    </div>
+    <div class="bg__front">
+        <div class="bg__menu">
+            <a class="menu__bio" href="#">Bio</a>
+            <a class="memu__work" href="#">Work</a>
+            <a class="menu__contact" href="#">Contact</a>
+        </div>
+        <img class="bg__logo" src='../assets/logo-250.png' alt='alex logo'>
+    </div>
+    <div class="bg__botton">
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg__back {
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+}
+.bg__front {
+    position: absolute;
+    z-index: 2;
+    top: 50px;
+    right: 50px;
+    width: calc(100vw - 100px);
+    height: calc(100vh - 100px);
+    background-color: #ff5851;
+}
+.bg__botton{
+    position: absolute;
+    z-index: 999;
+    bottom: 0;
+    right: 0;
+    width: 100vw;
+    height: 50px;
+    background-color: white;
+}
+.bg__logo{
+    position: absolute;
+    bottom:0px;
+    left: 0px;
+}
+.bg__menu{
+    position: absolute;
+    top: 0px;
+    right: 10px;
+}
+a{
+ text-decoration: none;
+ color: white;
+ margin: 0 10px;
+}
+</style>
